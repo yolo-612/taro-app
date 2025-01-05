736049c6dd410e86f7b62e6df389e3310b36780b
keruisiya <2458097476@qq.com>
2025-01-05 22:34:02 +0800

    feat(taro): taro代码初始化

---
+++ yolo-taro/src/app.config.ts
@@ -0,0 +1,13 @@
+export default defineAppConfig({
+  pages: [
+    'pages/index/index',
+    'pages/second/index'
+  ],
+  window: {
+    backgroundTextStyle: 'light',
+    navigationBarBackgroundColor: '#fff',
+    navigationBarTitleText: '铁蛋超人',
+    navigationBarTextStyle: 'black',
+    backgroundColor: "#F4F4F4"
+  }
+})
