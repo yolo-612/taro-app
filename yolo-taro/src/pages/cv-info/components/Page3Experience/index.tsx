1a8837e6b458e1436db119a4d9570f76ebdcac10
keruisiya <2458097476@qq.com>
2025-06-16 00:32:44 +0800

    feat(cv): css的class定义清晰

--- yolo-taro/src/pages/cv-info/components/Page3Experience/index.tsx
+++ yolo-taro/src/pages/cv-info/components/Page3Experience/index.tsx
@@ -4,7 +4,6 @@ import { View } from '@tarojs/components'
 import { useState } from 'react'
 import style from './index.module.scss'
 
-
 interface PropsType extends Experience {
   className?: string;
 }
@@ -18,24 +17,24 @@ export default function Experience({ className = '', expList }: PropsType) {
   }
 
   return (
-    <View className={`${style.experience} ${className}`}>
-      <View className={style.content}>
-        <View className={style.slider}>
+    <View className={`${style['experience']} ${className}`}>
+      <View className={style['content']}>
+        <View className={style['slider']}>
           <View className={style['slider-container']}>
             {/* 左侧图文 */}
-            <View className={style.left}>
+            <View className={style['left']}>
               <View
-                className={style.img}
+                className={style['img']}
                 style={{ backgroundImage: `url(${expCur.img})` }}
               />
-              <View className={style.title}>
+              <View className={style['title']}>
                 <h4>{expCur.title}</h4>
                 <p className={style['-post']}>"{expCur.post}"</p>
               </View>
             </View>
 
             {/* 右侧时间 + 列表 */}
-            <View className={style.right}>
+            <View className={style['right']}>
               <p className={style['-time']}>{expCur.time}</p>
               <View className={style['-list']}>
                 {expCur.contentList.map((item, index) => (
@@ -51,14 +50,14 @@ export default function Experience({ className = '', expList }: PropsType) {
         {/* 切换器 */}
         {expList.length > 1 && (
           <View className={style['shout-cut']}>
-            <View className={style.ul}>
+            <View className={style['ul']}>
               {expList.map((_, index) => (
                 <View
                   key={index}
                   className={
                     index === selectExpIndex
-                      ? `${style.li} ${style['-selected']}`
-                      : style.li
+                      ? `${style['li']} ${style['-selected']}`
+                      : style['li']
                   }
                   onClick={() => handleSwitch(index)}
                 />
--- yolo-taro/src/pages/cv-info/components/Page5Contact/index.tsx
+++ yolo-taro/src/pages/cv-info/components/Page5Contact/index.tsx
@@ -3,36 +3,35 @@ import type { Contact } from '@/store/modules/cvInfoSlice'
 import { View, Text, Image } from '@tarojs/components'
 import style from './index.module.scss'
 
-
 interface PropsType {
   className?: string;
   contact: Contact
   isEng?: boolean;
 }
 
-
 export default function Contact({ isEng = false, contact }: PropsType) {
   return (
-    <View className={style.contact}>
-      <View className={style.content}>
+    <View className={style['contact']}>
+      <View className={style['content']}>
         {/* 顶部描述 */}
-        <View className={`${style.words} ${isEng ? style['words-en'] : ''}`}>
+        <View className={`${style['words']} ${isEng ? style['words-en'] : ''}`}>
           {contact.desList1.map((item, index) => (
             <Text key={index} className={style['span']}>{item}</Text>
           ))}
         </View>
 
         {/* 中部想法描述 */}
-        <View className={style.idea}>
+        <View className={style['idea']}>
           {contact.desList2.map((item, index) => (
             <View key={index} className={style['idea-item']}>{item}</View>
           ))}
         </View>
 
         {/* 下载列表 */}
-        {/* <View className={style.download}>
-          <View className={style.title}>{contact.download}</View>
-          <View className={style.list}>
+        {/*
+        <View className={style['download']}>
+          <View className={style['title']}>{contact.download}</View>
+          <View className={style['list']}>
             {contact.fileList.map((item, index) => (
               <View key={index}>
                 <a href={item.url} target="_blank" rel="noreferrer">
@@ -41,10 +40,11 @@ export default function Contact({ isEng = false, contact }: PropsType) {
               </View>
             ))}
           </View>
-        </View> */}
+        </View>
+        */}
 
         {/* 联系方式（图标链接） */}
-        <View className={style.way}>
+        <View className={style['way']}>
           {contact.typeList.map((item, index) => (
             <a
               key={index}
